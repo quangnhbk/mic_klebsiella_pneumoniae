8ee23a4615c1bad4864b5d88996f273280bed204
quangnhbk <54691741+quangnhbk@users.noreply.github.com>
2022-12-20 21:48:34 +0700

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,10 +1,9 @@
 import os
 K = "8"
 
-main_dir = "/media/data/biodataset/KlebsiellaPneumoniae/" 
-for file_index, filename in enumerate(os.listdir(main_dir + "raw_data")):
-    # if file_index > 1: break
+main_dir = "./KlebsiellaPneumoniae/" 
+for file_index, filename in enumerate(os.listdir(main_dir + "raw_data")):    
     print("File number: ", file_index, filename)
-    os.system("/media/data/biodataset/KlebsiellaPneumoniae/tools/kmc -k" + K + " -ci0 -cs500000 -fm " + main_dir + "raw_data/" + filename + " ./counts_results_" + K + "/" + filename +  " ./")
-    os.system("/media/data/biodataset/KlebsiellaPneumoniae/tools/kmc_dump ./counts_results_" + K + "/" + filename +  " ./counts_data_" + K + "/" + '.'.join(filename.split('.')[0:-1]))
-    #break
+    os.system("./KlebsiellaPneumoniae/tools/kmc -k" + K + " -ci0 -cs500000 -fm " + main_dir + "raw_data/" + filename + " ./counts_results_" + K + "/" + filename +  " ./")
+    os.system("./KlebsiellaPneumoniae/tools/kmc_dump ./counts_results_" + K + "/" + filename +  " ./counts_data_" + K + "/" + '.'.join(filename.split('.')[0:-1]))
+    
